827a209ceae969b4c964ef484c20783781b7edc3
ItoShimeji <hamazakidaiti@gmail.com>
2026-05-26 15:34:01 +0900

    add

---
+++ practice/atcoder/abs/abc081_a/src/main.rs
@@ -0,0 +1,36 @@
+use std::io::{self, Read};
+
+struct Scanner {
+    input: Vec<String>,
+    index: usize,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut input = String::new();
+        io::stdin().read_to_string(&mut input).unwrap();
+        let input = input.split_whitespace().map(|s| s.to_string()).collect();
+
+        Scanner { input, index: 0 }
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        let value = self.input[self.index].parse::<T>().ok().unwrap();
+        self.index += 1;
+        value
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let input: String = sc.next();
+    // let a = b"
+
+    let output = input
+        .as_bytes()
+        .iter()
+        .filter(|&&x| x == "1".as_bytes()[0])
+        .count();
+
+    println!("{}", output);
+}
