1fb6db2e65f7df2790b2b41d8037a42f84a6a0aa
ItoShimeji <hamazakidaiti@gmail.com>
2026-05-27 23:28:19 +0900

    add

---
+++ practice/atcoder/abs/abc085_b/src/main.rs
@@ -0,0 +1,46 @@
+use std::collections::HashSet;
+use std::io::{self, Read};
+
+struct Scanner {
+    input: Vec<u8>,
+    index: usize,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut input = Vec::new();
+        io::stdin().read_to_end(&mut input).unwrap();
+
+        Scanner { input, index: 0 }
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        while self.index < self.input.len() && self.input[self.index].is_ascii_whitespace() {
+            self.index += 1;
+        }
+
+        let start = self.index;
+        while self.index < self.input.len() && !self.input[self.index].is_ascii_whitespace() {
+            self.index += 1;
+        }
+
+        std::str::from_utf8(&self.input[start..self.index])
+            .unwrap()
+            .parse::<T>()
+            .ok()
+            .unwrap()
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let n: usize = sc.next();
+
+    let mut set: HashSet<usize> = HashSet::new();
+
+    for _ in 0..n {
+        set.insert(sc.next());
+    }
+
+    println!("{}", set.len());
+}
