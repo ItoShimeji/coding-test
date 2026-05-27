8c7525d594eccd0ecd389a5cea4605adfd4e1089
ItoShimeji <hamazakidaiti@gmail.com>
2026-05-27 23:18:45 +0900

    add

---
+++ practice/atcoder/abs/abc088_b/src/main.rs
@@ -0,0 +1,41 @@
+use std::io::{self, Read};
+
+struct Scanner {
+    input: Vec<String>,
+    index: usize,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let mut input = String::new();
+        io::stdin().read_to_string(&mut input).unwrap();
+        let input = input.split_whitespace().map(|s| s.to_string()).collect();
+
+        Scanner { input, index: 0 }
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        let value = self.input[self.index].parse::<T>().ok().unwrap();
+        self.index += 1;
+        value
+    }
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let n: usize = sc.next();
+
+    let mut a: Vec<usize> = (0..n).map(|_| sc.next::<usize>()).collect();
+    a.sort();
+
+    let mut d = 0;
+    loop {
+        match (a.pop(), a.pop()) {
+            (Some(alice), Some(bob)) => d += alice - bob,
+            (Some(alice), None) => d += alice,
+            _ => break,
+        }
+    }
+
+    println!("{d}");
+}
