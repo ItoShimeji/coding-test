2998957b99a6ae602215a6c3831b14fc2f1d8204
ItoShimeji <hamazakidaiti@gmail.com>
2026-05-28 21:26:58 +0900

    add

--- practice/atcoder/adt_all/adt_all_20250114_3_d/src/main.rs
+++ practice/atcoder/adt_all/adt_all_20250114_3_d/src/main.rs
@@ -33,7 +33,31 @@ impl Scanner {
 
 fn main() {
     let mut sc = Scanner::new();
+    let n: usize = sc.next();
+    let edge_list = (0..n - 1)
+        .map(|_| (sc.next::<usize>(), sc.next::<usize>()))
+        .collect::<Vec<(usize, usize)>>();
 
-    // TODO: implement
-    let _ = &mut sc;
+    let first = edge_list[0];
+    let second = edge_list[1];
+
+    let root = if first.0 == second.0 || first.0 == second.1 {
+        first.0
+    } else {
+        first.1
+    };
+
+    // 与えられる構造は木であるため、2点間の辺の数は1つのため、rootが出てくる回数を単純に
+    // 数えて、それが n - 1 になることを示せばよいだけだが、下では調べてしまっている。
+    let mut exists = vec![false; n];
+    exists[root - 1] = true;
+    for &(v1, v2) in &edge_list {
+        let another = if v1 == root { v2 } else { v1 };
+
+        exists[another - 1] = true;
+    }
+
+    let is_yes = exists.iter().find(|&x| !x).is_none();
+
+    println!("{}", if is_yes { "Yes" } else { "No" });
 }
