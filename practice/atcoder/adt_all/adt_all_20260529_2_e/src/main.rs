c66008af454c191e7e31e7f5379640891a952330
ItoShimeji <hamazakidaiti@gmail.com>
2026-05-30 14:25:23 +0900

    add

--- practice/atcoder/adt_all/adt_all_20260529_2_e/src/main.rs
+++ practice/atcoder/adt_all/adt_all_20260529_2_e/src/main.rs
@@ -33,7 +33,36 @@ impl Scanner {
 
 fn main() {
     let mut sc = Scanner::new();
+    let n: usize = sc.next();
+    let a_list: Vec<isize> = (0..n).map(|_| sc.next::<isize>() * 2).collect();
+    let mut max_count = 0;
 
-    // TODO: implement
-    let _ = &mut sc;
+    for mask in 0..1usize << n {
+        let mut coord: isize = 1;
+        let mut count = 0;
+
+        for (i, &a) in a_list.iter().enumerate() {
+            let prev_coord = coord;
+
+            // bit が立っている場合は左に進む
+            if is_selected(mask, i) {
+                coord -= a;
+            } else {
+                coord += a;
+            }
+
+            // 符号が変わる場合はカウントを増加
+            if prev_coord < 0 && 0 < coord || 0 < prev_coord && coord < 0 {
+                count += 1;
+            }
+        }
+
+        max_count = max_count.max(count);
+    }
+
+    println!("{max_count}");
+}
+
+fn is_selected(mask: usize, i: usize) -> bool {
+    (mask & (1usize << i)) != 0
 }
