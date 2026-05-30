8c649449c91a98fd812d156c1733d85b7bf01838
ItoShimeji <hamazakidaiti@gmail.com>
2026-05-30 15:02:03 +0900

    add

--- practice/atcoder/adt_all/adt_all_20260529_2_f/src/main.rs
+++ practice/atcoder/adt_all/adt_all_20260529_2_f/src/main.rs
@@ -33,7 +33,33 @@ impl Scanner {
 
 fn main() {
     let mut sc = Scanner::new();
+    let mut s: Vec<u8> = sc.next::<String>().into_bytes();
 
-    // TODO: implement
-    let _ = &mut sc;
+    let mut i = 0;
+    let mut w_run_len = 0;
+    while i < s.len() - 1 {
+        if s[i] != b'W' {
+            w_run_len = 0;
+            i += 1;
+            continue;
+        } else {
+            w_run_len += 1;
+
+            if s[i + 1] == b'A' {
+                // i - w_run_len + 1 が A となり、
+                // そこから、 i + 1 までは C で埋める
+                s[i + 1 - w_run_len] = b'A';
+                for j in (i + 2 - w_run_len)..=(i + 1) {
+                    s[j] = b'C';
+                }
+
+                w_run_len = 0;
+                i += 2;
+            } else {
+                i += 1;
+            }
+        }
+    }
+
+    println!("{}", String::from_utf8(s).unwrap());
 }
