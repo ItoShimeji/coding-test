08ac8c92242bdb08756f3d086c688bc29378f2b4
ItoShimeji <hamazakidaiti@gmail.com>
2026-06-02 20:39:22 +0900

    add

--- practice/atcoder/typical90/typical90_003/src/main.rs
+++ practice/atcoder/typical90/typical90_003/src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{self, Read};
+use std::{
+    collections::VecDeque,
+    io::{self, Read},
+};
 
 struct Scanner {
     input: Vec<u8>,
@@ -33,7 +36,58 @@ impl Scanner {
 
 fn main() {
     let mut sc = Scanner::new();
+    let n: usize = sc.next();
+    let mut adj_list: Vec<Vec<usize>> = vec![Vec::new(); n];
+    for _ in 0..n - 1 {
+        let a: usize = sc.next();
+        let b: usize = sc.next();
 
-    // TODO: implement
-    let _ = &mut sc;
+        adj_list[a - 1].push(b);
+        adj_list[b - 1].push(a);
+    }
+
+    let (id, _) = bfs(&adj_list, 1);
+    let (_, d) = bfs(&adj_list, id);
+
+    println!("{}", d + 1);
+}
+
+fn bfs(adj_list: &[Vec<usize>], start: usize) -> (usize, usize) {
+    let n = adj_list.len();
+
+    let mut used: Vec<Vec<bool>> = (0..n)
+        .map(|idx_a| vec![false; adj_list[idx_a].len()])
+        .collect();
+
+    let mut d: Vec<usize> = vec![0; n];
+    let mut queue: VecDeque<usize> = VecDeque::new();
+    queue.push_back(start);
+
+    while let Some(a) = queue.pop_front() {
+        let idx_a = a - 1;
+        for (i, &b) in adj_list[idx_a].iter().enumerate() {
+            let idx_b = b - 1;
+            if used[idx_a][i] {
+                continue;
+            }
+            if d[idx_b] < d[idx_a] + 1 {
+                d[idx_b] = d[idx_a] + 1;
+                used[idx_a][i] = true;
+
+                for (j, &c) in adj_list[b - 1].iter().enumerate() {
+                    if c == a {
+                        used[b - 1][j] = true;
+                        break;
+                    }
+                }
+                queue.push_back(b);
+            }
+        }
+    }
+
+    d.iter()
+        .enumerate()
+        .map(|(i, &x)| (i + 1, x))
+        .max_by(|(_, a), (_, b)| a.cmp(b))
+        .unwrap()
 }
