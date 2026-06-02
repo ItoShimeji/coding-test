918e79d37336fd276b5968c93a090bf10cfb8ef2
ItoShimeji <hamazakidaiti@gmail.com>
2026-06-02 12:23:09 +0900

    add

--- practice/atcoder/typical90/typical90_001/src/main.rs
+++ practice/atcoder/typical90/typical90_001/src/main.rs
@@ -33,7 +33,60 @@ impl Scanner {
 
 fn main() {
     let mut sc = Scanner::new();
+    let n: usize = sc.next();
+    let l: usize = sc.next();
+    let k: usize = sc.next();
 
-    // TODO: implement
-    let _ = &mut sc;
+    // 左右の端も含めた切れ目の配列
+    let mut a_list: Vec<usize> = Vec::with_capacity(n + 2);
+    a_list.push(0);
+    for _ in 0..n {
+        a_list.push(sc.next());
+    }
+    a_list.push(l);
+
+    // それぞれの切れ目を使用するかどうかのフラグ
+    let mut flag = vec![true; n + 2];
+
+    // ある切れ目を取り除いたときに、従来の切れ目の左右の切れ目
+    // からなる領域の長さの最小
+    let mut min_len = usize::MAX;
+
+    for _ in 0..(n - k) {
+        min_len = usize::MAX;
+        let mut min_border = 1;
+
+        // 端の切れ目以外を走査
+        for i in 1..=n {
+            if !flag[i] {
+                continue;
+            }
+
+            // 新領域の左の border の位置
+            let x = {
+                let mut left = i - 1;
+                while !flag[left] {
+                    left -= 1;
+                }
+                a_list.get(left).copied().unwrap()
+            };
+
+            // 新領域の右の border の位置
+            let y = {
+                let mut right = i + 1;
+                while !flag[right] {
+                    right += 1
+                }
+                a_list.get(right).copied().unwrap()
+            };
+
+            if y - x < min_len {
+                min_len = y - x;
+                min_border = i;
+            }
+        }
+        flag[min_border] = false;
+    }
+
+    println!("{min_len}");
 }
