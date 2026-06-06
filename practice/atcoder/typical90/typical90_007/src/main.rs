810a639dad632f13634439faff17ce597ca06e77
ItoShimeji <hamazakidaiti@gmail.com>
2026-06-06 16:11:38 +0900

    add

--- practice/atcoder/typical90/typical90_007/src/main.rs
+++ practice/atcoder/typical90/typical90_007/src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{self, Read};
+use std::{
+    io::{self, Read},
+    usize,
+};
 
 struct Scanner {
     input: Vec<u8>,
@@ -33,7 +36,43 @@ impl Scanner {
 
 fn main() {
     let mut sc = Scanner::new();
+    let n: usize = sc.next();
+    let mut a_list: Vec<usize> = (0..n).map(|_| sc.next()).collect();
+    a_list.sort();
 
-    // TODO: implement
-    let _ = &mut sc;
+    let q: usize = sc.next();
+
+    'outer: for _ in 0..q {
+        let b: usize = sc.next();
+
+        let mut left = 0;
+        let mut right = n - 1;
+
+        if b <= a_list[left] {
+            println!("{}", a_list[left] - b);
+            continue;
+        }
+        if a_list[right] <= b {
+            println!("{}", b - a_list[right]);
+            continue;
+        }
+
+        while left + 1 < right {
+            let mid = left + (right - left) / 2;
+
+            if b < a_list[mid] {
+                right = mid;
+            } else if b == a_list[mid] {
+                println!("0");
+                continue 'outer;
+            } else {
+                left = mid;
+            }
+        }
+
+        let b_to_mid = b.abs_diff(a_list[left + 1]);
+        let b_to_left = b.abs_diff(a_list[left]);
+
+        println!("{}", b_to_mid.min(b_to_left));
+    }
 }
